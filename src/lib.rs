706a4b37fc9ba35c138e9cea1dd4ee7efaabda42
Rob Prestezog <robprestezog@gmail.com>
2024-12-11 13:22:40 -0800

    make modules public

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
-mod day11;
-mod day5;
-mod day4;
-mod day3;
-mod day2;
+pub mod day1;
+pub mod day11;
+pub mod day2;
+pub mod day3;
+pub mod day4;
+pub mod day5;
 extern crate aoc_runner;
 
 #[macro_use]
 extern crate aoc_runner_derive;
-
-pub mod day1;
 
 aoc_lib! { year = 2024 }
