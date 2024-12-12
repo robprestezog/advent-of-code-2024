cce5644db307bc32adadb6bd7cb6d45581a26853
Rob Prestezog <robprestezog@gmail.com>
2024-12-11 19:26:18 -0800

    optimization

--- src/day11.rs
+++ src/day11.rs
@@ -5,22 +5,20 @@ use aoc_runner_derive::aoc;
 #[aoc(day11, part1)]
 pub fn part1(input: &str) -> u64 {
     let mut count_lookup: HashMap<(u64, u8), u64> = HashMap::new();
-    let mut blink_lookup: HashMap<u64, Blink> = HashMap::new();
     input
         .split_ascii_whitespace()
         .map(|digits| digits.parse().unwrap())
-        .map(|num| count(num, 25, &mut count_lookup, &mut blink_lookup))
+        .map(|num| count(num, 25, &mut count_lookup))
         .sum()
 }
 
 #[aoc(day11, part2)]
 pub fn part2(input: &str) -> u64 {
     let mut count_lookup: HashMap<(u64, u8), u64> = HashMap::new();
-    let mut blink_lookup: HashMap<u64, Blink> = HashMap::new();
     input
         .split_ascii_whitespace()
         .map(|digits| digits.parse().unwrap())
-        .map(|num| count(num, 75, &mut count_lookup, &mut blink_lookup))
+        .map(|num| count(num, 75, &mut count_lookup))
         .sum()
 }
 
@@ -30,50 +28,39 @@ enum Blink {
     Two(u64, u64),
 }
 
-fn blink(stone: u64, blink_lookup: &mut HashMap<u64, Blink>) -> Blink {
-    blink_lookup
-        .entry(stone)
-        .or_insert({
-            if stone == 0 {
-                Blink::One(1)
-            } else {
-                let num_string = stone.to_string();
-                if num_string.len() % 2 == 0 {
-                    let (first, second) = num_string.split_at(num_string.len() / 2);
-                    Blink::Two(first.parse().unwrap(), second.parse().unwrap())
-                } else {
-                    Blink::One(stone * 2024)
-                }
-            }
-        })
-        .clone()
+fn blink(stone: u64) -> Blink {
+    match stone {
+        0 => Blink::One(1),
+        1..10 => Blink::One(stone * 2024),
+        10..100 => Blink::Two(stone / 10, stone % 10),
+        100..1000 => Blink::One(stone * 2024),
+        1000..10000 => Blink::Two(stone / 100, stone % 100),
+        10000..100000 => Blink::One(stone * 2024),
+        100000..1000000 => Blink::Two(stone / 1000, stone % 1000),
+        1000000..10000000 => Blink::One(stone * 2024),
+        10000000..100000000 => Blink::Two(stone / 10000, stone % 10000),
+        100000000..1000000000 => Blink::One(stone * 2024),
+        1000000000..10000000000 => Blink::Two(stone / 100000, stone % 100000),
+        10000000000..100000000000 => Blink::One(stone * 2024),
+        100000000000..1000000000000 => Blink::Two(stone / 1000000, stone % 1000000),
+        1000000000000..10000000000000 => Blink::One(stone * 2024),
+        10000000000000..100000000000000 => Blink::Two(stone / 10000000, stone % 10000000),
+        100000000000000..1000000000000000 => Blink::One(stone * 2024),
+        1000000000000000..10000000000000000 => Blink::Two(stone / 100000000, stone % 100000000),
+        _ => panic!("Likely overflow"),
+    }
 }
 
-fn count(
-    num: u64,
-    blinks: u8,
-    count_lookup: &mut HashMap<(u64, u8), u64>,
-    blink_lookup: &mut HashMap<u64, Blink>,
-) -> u64 {
+fn count(num: u64, blinks: u8, count_lookup: &mut HashMap<(u64, u8), u64>) -> u64 {
     if blinks == 0 {
         return 1;
-    }
-    if blinks == 1 {
-        if num.to_string().len() % 2 == 0 {
-            return 2;
-        } else {
-            return 1;
-        }
     }
     if let Some(c) = count_lookup.get(&(num, blinks)) {
         return *c;
     }
-    let c = match blink(num, blink_lookup) {
-        Blink::One(x) => count(x, blinks - 1, count_lookup, blink_lookup),
-        Blink::Two(x, y) => {
-            count(x, blinks - 1, count_lookup, blink_lookup)
-                + count(y, blinks - 1, count_lookup, blink_lookup)
-        }
+    let c = match blink(num) {
+        Blink::One(x) => count(x, blinks - 1, count_lookup),
+        Blink::Two(x, y) => count(x, blinks - 1, count_lookup) + count(y, blinks - 1, count_lookup),
     };
     count_lookup.insert((num, blinks), c);
     c
